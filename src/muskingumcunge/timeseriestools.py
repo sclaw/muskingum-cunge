719931fffc7dd80503bbd27a0c6898b577a2a5f3
sclaw <klawson1@uvm.edu>
2023-08-13 16:28:30 -0400

    add basin parabolic hydrograph

---
+++ src/muskingumcunge/timeseriestools.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def parabolic_hydrograph_generator(timestamps, peak, duration):
+    a = np.array([[(duration / 2) ** 2, duration / 2],
+                  [duration ** 2, duration]])
+    b = np.array([[peak],
+                  [0]])
+    coeffs = np.linalg.inv(a).dot(b)
+    flows = (coeffs[0] * (timestamps ** 2)) + (coeffs[1] * timestamps)
+    flows[flows < 0] = 0
+    return flows
