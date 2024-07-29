8bfdb8be12012b58566ce3589e4207d3fe7f7c13
sclaw <klawson1@uvm.edu>
2024-07-29 16:37:42 -0400

    catchup

--- samples/basin_analysis/run_all_basinwide.py
+++ samples/basin_analysis/run_all_basinwide.py
@@ -1,8 +1,13 @@
 import os
+import json
 from muskingumcunge.controller import handler
+import numpy as np
 
 run_dir = r'/netfiles/ciroh/floodplainsData/retrospective/basin_run_files'
 all_paths = [os.path.join(run_dir, f) for f in os.listdir(run_dir) if f.endswith('.json')]
 
 for path in all_paths:
+    with open(path, 'r') as f:
+        dict = json.load(f)
+
     handler(path)
